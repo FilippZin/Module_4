a84e9541ce5c6e3856024d3a8594e6bcf25ee279
FilippZin <filippwork777@gmail.com>
2024-10-06 15:22:44 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from Fake_math import divide as fak
+from True_math import divide as tru
+
+result1 = fak(69, 3)
+result2 = fak(3, 0)
+result3 = tru(49, 7)
+result4 = tru(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
